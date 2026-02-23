86eba481055ef1f9271acac18168a2058e734ab5
Cristian Campos <cristiancv2000@hotmail.com>
2026-02-22 19:42:39 -0500

    Listo para produccion

--- help_desk/settings.py
+++ help_desk/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*x-ag58tu2&pj7fiyviewd=68!a8hkd$$z=adl#xv8f8gu9ujj'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key-123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
